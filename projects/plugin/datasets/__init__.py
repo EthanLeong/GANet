d7fd73599d92459a6ddb2279078b3ec495fefa23
liang jinrong <liangjinrong@partner.sensetime.jp>
2023-01-20 14:06:10 +0900

    Added the data loading code for the once_2d lane dataset.

--- projects/plugin/datasets/__init__.py
+++ projects/plugin/datasets/__init__.py
@@ -1,4 +1,5 @@
 from .culane_dataset import CulaneDataset
 from .tusimple_dataset import TuSimpleDataset
 from .curvelane_dataset import CurvelaneDataset
+from .once2d_dataset import once2dDataset
 from .pipelines import *
